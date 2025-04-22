b88a4bc2ceaea7ba810a594b1bce3b0189c552f9
Fardias <fardias_alf@gmail.com>
2025-04-22 15:10:35 +0700

    add fix

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
             },
         ],
     },
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    },
 };
 
 export default nextConfig;
